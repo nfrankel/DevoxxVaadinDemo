98fc620b23e66bfddd2256af3c3674e0f417a3e3
Nicolas Frankel <nicolas@frankel.ch>
2012-11-12 09:58:05 +0100

    First behavior implementation

--- src/main/java/com/devoxx/vaadin/DevoxxUi.java
+++ src/main/java/com/devoxx/vaadin/DevoxxUi.java
@@ -1,7 +1,11 @@
 package com.devoxx.vaadin;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 
 
@@ -12,9 +16,26 @@ public class DevoxxUi extends UI {
 
 		HorizontalLayout layout = new HorizontalLayout();
 
-		Label label = new Label("Hello Devoxx!");
+		final TextField field = new TextField();
+
+		layout.addComponent(field);
+
+		Button button = new Button("Say hello");
+
+		layout.addComponent(button);
+
+		final Label label = new Label("Hello Devoxx!");
 
 		layout.addComponent(label);
+
+		button.addClickListener(new ClickListener() {
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+
+				label.setValue("Hello " + field.getValue() + "!");
+			}
+		});
 
 		setContent(layout);
 	}
