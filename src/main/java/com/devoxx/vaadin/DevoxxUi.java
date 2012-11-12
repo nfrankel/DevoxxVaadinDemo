e69f4a4d4eccac9a95776e30e78bb34b3be510d9
Nicolas Frankel <nicolas@frankel.ch>
2012-11-12 10:16:15 +0100

    Preserve state on refresh

--- src/main/java/com/devoxx/vaadin/DevoxxUi.java
+++ src/main/java/com/devoxx/vaadin/DevoxxUi.java
@@ -1,10 +1,11 @@
 package com.devoxx.vaadin;
 
 import com.devoxx.vaadin.view.MainView;
+import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.UI;
 
-
+@PreserveOnRefresh
 public class DevoxxUi extends UI {
 
 	@Override
